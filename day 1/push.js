dfe5b6ecf0bf4f3457c25927a60405f4dacd1049
abraranik-sudo <61088935+abraranik-sudo@users.noreply.github.com>
2022-11-02 19:52:19 +0600

    Add files via upload

---
+++ day 1/push.js
@@ -0,0 +1,12 @@
+// ekta array te onno arrey er value dhukanor jnno use kora hoy, array er vitor push use kore add korte hoy 
+
+// exmpl:
+
+let person_one = [ 'sajid', 'mojid'];
+
+//let person_two = ['goni', 'moni'];  ==> second array
+
+
+
+let person_two = [...person_one, 'goni','moni'];
+person_two.push(person_one);
